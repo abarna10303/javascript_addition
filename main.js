9dc1910d0b57ff2f746429c9975c77c89ba275db
abarna10303 <abarnas361@gmail.com>
2022-12-13 16:49:23 +0530

    Addition

---
+++ main.js
@@ -0,0 +1,7 @@
+/*ADDITION PROGRAM*/
+var num1,num2,add;
+num1=parseInt(prompt("Enter the First Number"));
+num2=parseInt(prompt("Enter the second Number"));
+add=num1+num2;
+console.log("Sum of Two number is : " +add);
+
